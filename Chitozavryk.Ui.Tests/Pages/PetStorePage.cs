8a0414471b614e0ddbea8a657a1740dc4168f9de
Marianna Senyk <senikmarianna@gmail.com>
2026-04-14 13:28:59 +0200

    Refactor: add first UI test

---
+++ Chitozavryk.Ui.Tests/Pages/PetStorePage.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Playwright;
+
+namespace Chitozavryk.Ui.Tests.Pages
+{
+	public class PetStorePage
+	{
+		private readonly IPage _page;
+
+		private ILocator CookiesBtn => _page.GetByRole(AriaRole.Button, new() { Name = "Allow all cookies" });
+		private ILocator GetPetByIdSection => _page.GetByRole(AriaRole.Button, new() { Name = "GET /pet/{petId} Find pet by" });
+		private ILocator TryItOutBtn => _page.GetByRole(AriaRole.Button, new() { Name = "Try it out" });
+		private ILocator PetIdInput => _page.GetByRole(AriaRole.Textbox, new() { Name = "petId" });
+		private ILocator ExecuteBtn => _page.GetByRole(AriaRole.Button, new() { Name = "Execute" });
+
+		public ILocator ResponseResult => _page.GetByRole(AriaRole.Row, new() { Name = "200 successful operation" }).Locator("pre");
+
+		public PetStorePage(IPage page) => _page = page;
+
+		public async Task OpenAsync() => await _page.GotoAsync("https://petstore.swagger.io/");
+
+		public async Task FindPetByIdAsync(string id)
+		{
+			if (await CookiesBtn.IsVisibleAsync())
+			{
+				await CookiesBtn.ClickAsync();
+			}
+
+			await GetPetByIdSection.ClickAsync();
+			await TryItOutBtn.ClickAsync();
+			await PetIdInput.FillAsync(id);
+			await ExecuteBtn.ClickAsync();
+		}
+
+	}
+}
---
+++ Chitozavryk.Ui.Tests/Tests/PetStoreTests.cs
@@ -0,0 +1,20 @@
+﻿using Chitozavryk.Ui.Tests.Pages;
+
+using Microsoft.Playwright.Xunit;
+
+namespace Chitozavryk.Ui.Tests.Tests
+{
+	public class PetStoreTests : PageTest
+	{
+		[Fact]
+		public async Task ShouldFindPetById_WhenIdIsValid()
+		{
+			var petPage = new PetStorePage(Page);
+
+			await petPage.OpenAsync();
+			await petPage.FindPetByIdAsync("1");
+
+			await Expect(petPage.ResponseResult).ToContainTextAsync("id");
+		}
+	}
+}
